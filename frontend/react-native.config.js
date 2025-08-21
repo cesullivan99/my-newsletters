78db33b76528824ef0396aa34c3f161c3d1fde35
cesullivan99 <cesullivan99@gmail.com>
2025-08-21 11:31:15 -0400

    IOS build fixes

---
+++ frontend/react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  project: {
+    ios: {
+      automaticPodsInstallation: true
+    },
+    android: {}
+  },
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
+};
